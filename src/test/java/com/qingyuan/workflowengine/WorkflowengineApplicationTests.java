4153da9b0c28308f99540f911fee89245ed86d24
guoshusong <2460526449@qq.com>
2020-09-05 20:23:37 +0800

    工作流引擎

---
+++ src/test/java/com/qingyuan/workflowengine/WorkflowengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingyuan.workflowengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowengineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
